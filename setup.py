e72a27b50268cd77fb15f662e4e96d9921862d4e
merrillkoshy <merrillkoshy@gmail.com>
2018-08-18 21:26:02 +0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="<program_name>",
+    version="<version>",
+    author_email="<email>",
+    description="<description>,
+    py_modules = ['<filename>']
+)
