cebe1bc8586c3b752e7193d4c15dc6c23520e8f4
merrillkoshy <merrillkoshy@gmail.com>
2018-08-24 16:50:10 +0400

    Update views.py

--- src/dt/views.py
+++ src/dt/views.py
@@ -24,7 +24,7 @@ def contact_page(request):
 	contact_form=ContactForm(request.POST or None)
 	context={
 		"title":"con",
-		"content":"ariyana?nink ariyanaa!!_nn",
+		"content":"ariyana?nink ariyanaann",
 		"form":contact_form
 	}
 	if contact_form.is_valid():
