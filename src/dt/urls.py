fc25f96e15353fdb14f07bff980637aa530cd0a3
merrillkoshy <merrillkoshy@gmail.com>
2018-08-27 17:46:06 +0400

    added products n stuff

--- src/dt/urls.py
+++ src/dt/urls.py
@@ -17,9 +17,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
-
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
 
 from .views import home_page,about_page,contact_page,login_page,register_page
 
@@ -30,6 +30,10 @@ urlpatterns = [
 	path('contact/', contact_page),
 	path('login/', login_page),
 	path('register/', register_page),
+    path('products/', ProductListView.as_view()),
+    path('products-fbv/', product_list_view ),
+    path('products/<int:pk>/', ProductDetailView.as_view()),
+    path('products-fbv/<int:pk>/', product_detail_view ),
     path(r'admin/', admin.site.urls),
 ]
 
