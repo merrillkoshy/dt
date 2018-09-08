04852a7abc426470fa00770627260096af4a7a73
MerrillKoshy <merrillkoshy@gmail.com>
2018-09-08 21:46:00 +0400

    Productfn

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.views.generic import ListView, DetailView 
 from django.shortcuts import render,get_object_or_404
 
@@ -6,7 +7,7 @@ from .models import Product
 
  
 class ProductListView(ListView):
-	queryset=Product.objects.all()
+	#queryset=Product.objects.all()
 	template_name ="products/list.html"
 
 	# def get_context_data(self, *args, **kwargs):
@@ -14,7 +15,9 @@ class ProductListView(ListView):
 	# 	print(context)
 	# 	return context
 
-
+	def get_queryset(self,*args,**kwargs):
+		request=self.request
+		return Product.objects.all()
 
 def product_list_view(request):
 	queryset=Product.objects.all()
@@ -31,14 +34,44 @@ class ProductDetailView(DetailView):
 	def get_context_data(self, *args, **kwargs):
 		context=super(ProductDetailView,self).get_context_data(*args,**kwargs)
 		print(context)
-		context['abc']=123
+		#context['abc']=123
 		return context
 
+	def get_object(self,*args,**kwargs):
+		request =self.request	
+		pk=self.kwargs.get('pk')
+		instance=Product.objects.get_by_id(pk)
+		if instance is None:
+			raise Http404("Product doesnt exist")
+		return instance
+
+	# def get_queryset(self,*args,**kwargs):
+	# 	request=self.request
+	# 	return Product.objects.filter(pk=pk)	
 
 
 def product_detail_view(request, pk=None, *args, **kwargs):	
 	# instance=Product.objects.get(pk=pk) #id
-	instance=get_object_or_404(Product, pk=pk)
+	# try:
+	# 	instance=Product.objects.get(Product, id=pk)
+	# except Product.DoesNotExist:
+	# 	print('no product here')
+	# 	raise Http404("Product doesnt exist")
+	# except:
+	# 	print("huh?")
+
+	instance=Product.objects.get_by_id(pk)
+	if instance is None:
+		raise Http404("Product doesnt exist")
+	#print(instance)
+	# qs=Product.objects.filter(id=pk) 
+	
+	# #print(qs)
+	# if qs.exists() and qs.count()==1:
+	# 	instance=qs.first()
+	# else:
+	# 	raise Http404("Product doesnt exist")	
+
 	context={
 		'object':instance
 	}
